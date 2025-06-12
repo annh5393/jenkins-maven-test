388ec7ecd1d032b507015063987c811899526462
annh5393 <annh5393@gmail.com>
2025-06-12 10:27:45 +0700

    Test linters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    // Thêm constructor private để ngăn tạo đối tượng từ lớp này
+    private Main() {
+        // Utility class should not be instantiated
+    }
+
+    /**
+     * Entry point of the application.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world!");
     }
 }
